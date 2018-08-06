6baf77f5448d24af3dd76afa50c71f3fb629d822
rbhirve <rbhirve@gmail.com>
2018-08-06 23:46:24 +0530

    view engin set ejs
    Signed-off-by: rbhirve <rbhirve@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log(req.body);
   res.send('respond with a resource');
 });
 
